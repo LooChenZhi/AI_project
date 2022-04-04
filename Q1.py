0292e97687de70172c99dbfc523c7ef5d47c7496
LooChenZhi <102901234+LooChenZhi@users.noreply.github.com>
2022-04-04 15:09:33 +0800

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -18,12 +18,23 @@ import streamlit as st
 
 
 # Individual Function
+st.title("Question 1: Vacation Planner")
+st.markdown("Using Genetic Algorithm (GA) to solve a vacation planning. The goal is to optimize the vacation experience with a fixed amount of money and fixed duration. This application will generate the best combination of values for each parameter.")
 
-# In[ ]:
+st.sidebar.header("Change the values here")
+money_on_hand = st.sidebar.slider("Money On-Hand (RM)", min_value = 4000, max_value = 10000, value = 1000, step = 1000)
+duration = st.sidebar.slider("Duration (Day)", min_value = 3, max_value = 10, value = 3, step = 1)
+p_count = st.sidebar.slider("Population", min_value = 100, max_value = 1000, value = 100, step = 100)
+n_generation = st.sidebar.slider("Generation", min_value = 100, max_value = 1000, value = 200, step = 100)
+data = {"Budget" : money_on_hand,
+        "Duration" : duration,
+        "population" : p_count,
+        "n_generation" : n_generation}
 
-
-money_on_hand = 5000    #budget
-duration = 5            #5days4night
+money_on_hand = int(money_on_hand) #budget
+duration = int(duration)
+p_count = int(p_count)
+n_generation = int(n_generation)
 
 # Create an individual list. An individual is a member of a population.
 # Range each parameter
@@ -40,10 +51,6 @@ def individual():
 
 
 # Population function
-
-# In[ ]:
-
-
 # Create population contains individuals
 # The collection of all individuals is referred to as our population.
 def population(count):
@@ -51,10 +58,6 @@ def population(count):
 
 
 # Fitness function
-
-# In[ ]:
-
-
 # Count the fitness function for an individual
 # Calculate the range between money on hand with total
 def fitness(individual):
@@ -65,9 +68,6 @@ def fitness(individual):
 
 # Average fitness function
 
-# In[ ]:
-
-
 # Calculate the fitness function to retrieve average fitness for a population
 def average_fitness(pop):
     summed = [fitness(i) for i in pop]
@@ -75,11 +75,7 @@ def average_fitness(pop):
 
 
 # Evolution Function
-
-# In[ ]:
-
-
-def evolve(pop, retain = 0.2, random_select = 0.05, mutate = 0.01):
+def evolve(pop, retain, crossover1, crossover2, mutate, random_select = 0.05):
     
     graded = [(fitness(x), x) for x in pop]
     sort_graded = [x[1]for x in sorted(graded)]  # x [1] because x has two component, just take the list --> e.g. [(50,[41,38,86,30,55])]
@@ -108,46 +104,37 @@ def evolve(pop, retain = 0.2, random_select = 0.05, mutate = 0.01):
             male = parents[male]
             female = parents[female]
             half = int(len(male)/2)
-            child = male[:half] + female[half:]
+            child = male[:crossover1] + female[crossover2:]
             children.append(child)
     parents.extend(children)
     
     return parents
 
 
-# Result
+def gen():
+    # Result
+    # Decalaration of list
+    value_lst =[]
+    fitness_history = []
 
-# In[ ]:
+    p = population(p_count)
 
+    # Iterate and modeling for result
+    for i in range(n_generation):
+        p = evolve(p, retain, crossover1, crossover2, mutate)
+        value = average_fitness(p)
+        fitness_history.append(value)
+        value_lst.append(p[0])
+        value_lst.append(value)
 
-# Decalaration of list
-value_lst =[]
-fitness_history = []
+    #st.write(value_lst) #print result
 
-p_count = 100
-n_generation = 100
-
-p = population(p_count)
-
-# Iterate and modeling for result
-for i in range(n_generation):
-    p = evolve(p)
-    value = average_fitness(p)
-    fitness_history.append(value)
-    value_lst.append(p[0])
-    value_lst.append(value)
-
-st.write(value_lst) #print result
+    best_model = value_lst[-2]
+    best_fit = value_lst[-1]
 
 
-# In[ ]:
-
-
-best_model = value_lst[-2]
-
-def best_solution():
     # [hotel,food,t_spots,one_t_spots,t_fee,t_fre]
-    st.write("Best Solution :")
+    st.write("**Best Solution :**")
     st.write("Money on hand       = RM",money_on_hand)
     st.write("Vacation duration   =",duration,"days")
     st.write("Hotel Price         = RM", best_model[0])
@@ -157,33 +144,73 @@ def best_solution():
     st.write("Transport Fee       = RM", best_model[4])
     st.write("Transport Frequency =", best_model[5],"trip per day")
 
-best_solution()
 
+    total = best_model[0]*4 + best_model[1]*3*duration + best_model[2]*best_model[3] + best_model[4]*best_model[5]*duration
+    st.write("Total expenses: RM", total)
 
-# In[ ]:
+    df_hist = pd.DataFrame()
+    df_hist["Generation"] = [x + 1 for x in list(range(len(fitness_history)))]
+    df_hist["Fitness History"] = fitness_history
 
+    return fitness_history, best_fit, df_hist
 
-total = best_model[0]*4 + best_model[1]*3*duration + best_model[2]*best_model[3] + best_model[4]*best_model[5]*duration
-st.write("Total expenses: RM", total)
+st.write("# Method 1")
+retain = 0.2
+mutate = 0.02
+crossover1 = 2
+crossover2 = 2
+st.write("Selection: 20%")
+st.write("Mutate Position: 2")
+st.write("From Male: First 2")
+st.write("From Female: Last 2")
+st.write(" ")
+hist1, fit1, df1 = gen()
+st.markdown("""---""")
 
+st.write("# Method 2")
+retain = 0.4
+mutate = 0.04
+crossover1 = 4
+crossover2 = 4
+st.write("Selection: 40%")
+st.write("Mutate Position: 4")
+st.write("From Male: First 4")
+st.write("From Female: Last 4")
+st.write(" ")
+hist2, fit2, df2 = gen()
+st.markdown("""---""")
 
-# Plotting Chart
+st.write("# Method 3")
+retain = 0.1
+mutate = 0.01
+crossover1 = 1
+crossover2 = 1
+st.write("Selection: 10%")
+st.write("Mutate Position: 1")
+st.write("From Male: First 1")
+st.write("From Female: Last 1")
+st.write(" ")
+hist3, fit3, df3 = gen()
+st.markdown("""---""")
 
-# In[ ]:
+st.write("# Compare the Results")
+merged_df = pd.merge(df2, df3, how = "outer", on = ["Generation"])
+merged_df = pd.merge(merged_df, df1, how = "outer", on = ["Generation"])
+merged_df.columns = ["Generation", "Method 1", "Method 2", "Method 3"]
+merged_df
+st.write(" ")
 
+#Plot graph
+fig, ax = plt.subplots(figsize = (10, 5))
 
-xval = np.arange(1, 101)
-yval = fitness_history
+plt.plot(hist1, label = "Method 1")
+plt.plot(hist2, label = "Method 2")
+plt.plot(hist3, label = "Method 3")
+plt.legend()
 
-fig = plt.figure(figsize=(20,10))
-plt.subplot(122)
-plt.axhline(y=0, color='r', linestyle='-')
-plt.plot(yval, color='c')
-plt.xlim(-5)
-
-plt.xlabel('Number of generation')
-plt.ylabel('Fitness')
-plt.title('Fitness vs Number of generation')
+ax.set_xlabel("Generation")
+ax.set_ylabel("Fitness")
+ax.set_title("Fitness over Generation")
 st.pyplot(fig)
 
 
